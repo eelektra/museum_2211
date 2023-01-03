c69f3f375def23840e9b5fb5b51b3c6db61da302
Carol Rieping <carol.rieping@gmail.com>
2023-01-03 09:41:15 -0700

    Add interests test

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,8 +1,16 @@
 class Patron
 
+  attr :name,
+       :spending_money,
+       :interests
+
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
     @interests = []
+  end
+
+  def add_interest(interest)
+    @interests << interest
   end
 end
