f47b23c64d8694cb9b8811035af57d17808419dd
Carol Rieping <carol.rieping@gmail.com>
2023-01-03 10:57:21 -0700

    Add Iter 3 tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,11 +1,15 @@
 class Museum
 
   attr :name,
-       :exhibits
+       :exhibits,
+       :patrons,
+       :patrons_by_exhibit
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
+    @patrons_by_exhibit = {}
   end
 
   def add_exhibit(exhibit)
@@ -14,5 +18,13 @@ class Museum
 
   def recommend_exhibits(patron)
     patron.interests
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    @patrons_by_exhibit[exhibit] = patron
   end
 end
