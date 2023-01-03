1ac8a8ab2620b43df400fa5eb3e1aef9ac7b7923
Carol Rieping <carol.rieping@gmail.com>
2023-01-03 10:10:54 -0700

    Add exhibit tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -2,9 +2,13 @@ class Museum
 
   attr :name,
        :exhibits
-       
+
   def initialize(name)
     @name = name
     @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
   end
 end
